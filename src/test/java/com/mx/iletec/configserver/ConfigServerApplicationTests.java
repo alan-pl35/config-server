e89c0a33366ae50349b8a996fce3fcd07de5a729
alan-pl35 <alan.puga35@outlook.com>
2024-04-10 20:21:40 -0600

    el proyecto fue agregado al repo exitosamente

---
+++ src/test/java/com/mx/iletec/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.iletec.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
